be00b37fa634ae18965a8b23341b3c9d628184fc
simonsg7 <ssernag7@gmail.com>
2025-04-05 14:04:06 -0500

    Button1 updated for add the function onClick

--- src/components/Button1/index.jsx
+++ src/components/Button1/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Button } from 'primereact/button';
 
-const Button1 = ({ className, label }) => {
+const Button1 = ({ className, label, onClick }) => {
     return (
         <>
-            <Button text raised rounded className={`${className} bg-primary-light text-tertiary-light hover:bg-white hover:text-primary-light p-[0.7rem]`} label={`${label}`} />
+            <Button text raised rounded className={`${className} bg-primary-light text-tertiary-light hover:bg-white hover:text-primary-light p-[0.7rem]`} label={`${label}`} onClick={onClick} />
         </>
     );
 }
