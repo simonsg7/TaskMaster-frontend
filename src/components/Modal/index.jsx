c2ee48827d51b0ce197b2c345147804c3466750d
simonsg7 <ssernag7@gmail.com>
2025-07-14 16:57:54 -0500

    Component Modal for to create a new card in KanbanBoard

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -18,7 +18,7 @@ const Modal = ({ title, children, buttonName }) => {
 
             {isOpen && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg w-auto max-w-[45rem] relative">
+                    <div className="bg-white p-6 pb-0 rounded-lg shadow-lg w-auto max-w-[45rem] relative flex flex-col items-center justify-center">
                         {title && <h2 className="text-xl font-bold mb-4">{title}</h2>}
 
                         <button onClick={handleClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
