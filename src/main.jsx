d0b30f9a0f37e6781d9606a1281758c3b006094d
simonsg7 <ssernag7@gmail.com>
2025-07-29 06:12:04 -0500

    React-Query added and installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.jsx';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+);
