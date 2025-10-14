9b258dea40a119a993d04a15f9255101a58c9d92
simonsg7 <ssernag7@gmail.com>
2025-10-14 18:24:46 -0500

    fix: Fix CORS and routing errors on Vercel, the error eas due to a double slash ('//') in the API URL, and add vercel.json file

--- src/api/backendUrls.js
+++ src/api/backendUrls.js
@@ -1,15 +1,15 @@
-const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3000/';
 
 const userId = localStorage.getItem('userId');
 
-export const urlLogin = `${baseUrl}/auth/login`;
-export const urlGetAllUsers = `${baseUrl}/users/all`;
-export const urlMyProfile = (userId) => `${baseUrl}/users/byid/${userId}`;
-export const urlUpdateProfileImage = (userId) => `${baseUrl}/users/update-profile-image/${userId}`;
-export const urlProjectsByUserId = (userId) => `${baseUrl}/projects/projectsbyuserid/${userId}`;
-export const urlCreateTask = `${baseUrl}/tasks/create`;
-export const urlUpdateTask = (taskId) => `${baseUrl}/tasks/update/${taskId}`;
-export const urlDeleteTask = (taskId) => `${baseUrl}/tasks/delete/${taskId}`;
-export const urlCreateProject = `${baseUrl}/projects/create`;
-export const urlUpdateProject = (projectId) => `${baseUrl}/projects/update/${projectId}`;
-export const urlDeleteProject = (projectId) => `${baseUrl}/projects/delete/${projectId}`;
+export const urlLogin = `${baseUrl}auth/login`;
+export const urlGetAllUsers = `${baseUrl}users/all`;
+export const urlMyProfile = (userId) => `${baseUrl}users/byid/${userId}`;
+export const urlUpdateProfileImage = (userId) => `${baseUrl}users/update-profile-image/${userId}`;
+export const urlProjectsByUserId = (userId) => `${baseUrl}projects/projectsbyuserid/${userId}`;
+export const urlCreateTask = `${baseUrl}tasks/create`;
+export const urlUpdateTask = (taskId) => `${baseUrl}tasks/update/${taskId}`;
+export const urlDeleteTask = (taskId) => `${baseUrl}tasks/delete/${taskId}`;
+export const urlCreateProject = `${baseUrl}projects/create`;
+export const urlUpdateProject = (projectId) => `${baseUrl}projects/update/${projectId}`;
+export const urlDeleteProject = (projectId) => `${baseUrl}projects/delete/${projectId}`;
