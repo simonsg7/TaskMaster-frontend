00d89bd0542d4d84295360ffad30c29f820b75d6
simonsg7 <ssernag7@gmail.com>
2025-10-27 15:42:43 -0500

    Add register button to login and add title prop to Button1

--- src/components/Button1/index.jsx
+++ src/components/Button1/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Button } from 'primereact/button';
 
-const Button1 = ({ className, label, onClick }) => {
+const Button1 = ({ className, label, onClick, title }) => {
     
     return (
         <>
-            <Button text raised rounded className={`${className} bg-primary-light text-tertiary-light hover:bg-white hover:text-primary-light p-[0.7rem]`} label={`${label}`} onClick={onClick} />
+            <Button text raised rounded className={`${className} bg-primary-light text-tertiary-light hover:bg-white hover:text-primary-light p-[0.7rem]`} title={`${title}`} label={`${label}`} onClick={onClick} />
         </>
     );
 }
--- src/pages/Auth/Login/index.jsx
+++ src/pages/Auth/Login/index.jsx
@@ -2,15 +2,19 @@ import { useForm } from "react-hook-form";
 import { InputText } from 'primereact/inputtext';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 import './Login.scss';
 import logo from '../../../assets/Captura_de_pantalla_2024-11-15_093642-removebg-preview.png';
 import { urlLogin } from "../../../api/backendUrls";
 import { handleApiErrors } from "../../../api/handleApiErrors";
 import Button1 from "../../../components/Button1";
+import FormRegister from "../../../components/FormRegister";
+import Modal from "../../../components/Modal";
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
     const navigate = useNavigate();
 
     const onSubmit = async (data) => {
@@ -21,7 +25,7 @@ const Login = () => {
             localStorage.setItem("userDetailId", (response.data.user.user_detail_id));         
             localStorage.setItem("imageUrl", response.data.user.image_url);
             console.log(response);
-            navigate('/');
+            navigate('/profile');
         } catch (error) {
             const errorResponse = handleApiErrors(error);
             console.error("Error during login:", errorResponse.message);
@@ -35,22 +39,36 @@ const Login = () => {
                 <img src={logo} alt="Logo" className='w-[40rem] h-auto' />
             </div>
             <div className='inferior-div flex justify-center items-center bg-tertiary-light rounded-t-[100px]'>
-                <form onSubmit= {handleSubmit(onSubmit)} className="w-[22rem] flex flex-col items-center p-[3rem]">
-                    <div className="p-2">
-                        <InputText {...register("email", { required: true })} type="email" className="p-[0.3rem] border border-blue-300 hover:bg-bgInput" placeholder="Email" />
-                        <br />
-                        {errors.email && (<span className="text-[0.85rem]">Is required</span>)}
+                <form onSubmit= {handleSubmit(onSubmit)} className="w-[30rem] flex flex-col items-center p-[3rem]">
+                    <div className="w-[20rem] flex flex-col items-center">
+                        <div className="p-2">
+                            <InputText {...register("email", { required: true })} type="email" className="p-[0.3rem] border border-blue-300 hover:bg-bgInput" placeholder="Email" />
+                            <br />
+                            {errors.email && (<span className="text-[0.85rem]">Is required</span>)}
+                        </div>
+
+                        <div className="p-2">
+                            <InputText {...register("password", { required: true })} type="password" className="p-[0.3rem] border border-blue-300 hover:bg-bgInput" placeholder="Password" />
+                            <br />
+                            {errors.password && (<span className="text-[0.85rem]">Is required</span>)}
+                        </div>
                     </div>
 
-                    <div className="p-2">
-                        <InputText {...register("password", { required: true })} type="password" className="p-[0.3rem] border border-blue-300 hover:bg-bgInput" placeholder="Password" />
-                        <br />
-                        {errors.password && (<span className="text-[0.85rem]">Is required</span>)}
+                    <div className="w-[20rem] flex justify-around">
+                        <div>
+                            <Button1 className="w-[7.5rem] mt-[0.5rem] mb-[1rem]" label="Login" /> 
+                        </div>
+                        <div>
+                            <Button1 className="w-[7.5rem] mt-[0.5rem] mb-[1rem]" label="Register" title="Don't you have an account?" onClick={() => setIsRegisterModalOpen(true)} />
+                        </div>
                     </div>
-
-                    <Button1 className="w-[7.5rem] mt-[1rem]" label="Login" />
                 </form>
             </div>
+            <Modal title="Register" isOpen={isRegisterModalOpen} onClose={() => setIsRegisterModalOpen(false)}>
+                {({ close }) => (
+                    <FormRegister close={close} />
+                )}
+            </Modal>
         </div>
     );
 }
